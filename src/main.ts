5428ef96f6e47f02d66898f8bc64edf455cb9b55
EmnaMbarki94 <Emna.Mbarki@esprit.tn>
2025-08-16 15:48:02 +0100

    Removed KYC_Project extra files

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/components/app.component';
-import 'zone.js';  // 👈 très important
-import { appConfig } from './app/components/app.config';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch(err => console.error(err));
